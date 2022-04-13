1e6a489fecbd746771e3cf46ae9e76bbf3b47760
Neha Sen <neha712513@gmail.com>
2022-04-14 01:45:39 +0530

    i

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require("./index");
 const connect = require("./config/db");
 
-app.listen(4000, async() => {
+app.listen(4123, async() => {
     try{
         await connect();
-        console.log("listening on port 4000");
+        console.log("listening on port 4123");
     }
     catch(err) {
         console.log('err', err)
